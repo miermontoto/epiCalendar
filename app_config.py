1bfd0886586e2aafb0574a66da1a314d44e19ad9
miermontoto <underscoreBis@gmail.com>
2022-09-07 02:43:27 +0200

    allow org accounts by changing read scope

--- app_config.py
+++ app_config.py
@@ -4,7 +4,7 @@ CLIENT_ID = "250353b2-562b-4e19-83ff-6abc7ce3e925"
 AUTHORITY = "https://login.microsoftonline.com/common"
 REDIRECT_PATH = "/getAToken"
 ENDPOINT = 'https://graph.microsoft.com/v1.0/users'
-SCOPE = ["User.Read.All"]
+SCOPE = ["User.ReadBasic.All"]
 SESSION_TYPE = "filesystem"
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
